f862b6584705e0aed9285755486f1b6af75a8086
Stephanie <stephaniiehallberg@gmail.com>
2024-08-04 16:16:56 +0200

    8. Writing unit tests

---
+++ src/users/tests/users.service.spec.ts
@@ -0,0 +1,46 @@
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import User from '../user.entity';
+import { UsersService } from '../users.service';
+
+describe('The UsersService', () => {
+  let usersService: UsersService;
+  let findOneBy: jest.Mock;
+  beforeEach(async () => {
+    findOneBy = jest.fn();
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            findOneBy
+          }
+        }
+      ],
+    })
+      .compile();
+    usersService = await module.get(UsersService);
+  })
+  describe('when getting a user by email', () => {
+    describe('and the user is matched', () => {
+      let user: User;
+      beforeEach(() => {
+        user = new User();
+        findOneBy.mockReturnValue(Promise.resolve(user));
+      })
+      it('should return the user', async () => {
+        const fetchedUser = await usersService.getByEmail('test@test.com');
+        expect(fetchedUser).toEqual(user);
+      })
+    })
+    describe('and the user is not matched', () => {
+      beforeEach(() => {
+        findOneBy.mockReturnValue(undefined);
+      })
+      it('should throw an error', async () => {
+        await expect(usersService.getByEmail('test@test.com')).rejects.toThrow();
+      })
+    })
+  })
+});
